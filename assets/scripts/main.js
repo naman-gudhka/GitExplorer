6876a7862ba243f09b608a3f4f8d5d5916285af4
naman-gudhka <naman.gudhka19@gmail.com>
2026-09-14 14:26:11 +0530

    Implement theme toggle with localStorage persistence

---
+++ assets/scripts/main.js
@@ -0,0 +1,21 @@
+import { loadFromStorage, saveToStorage } from "./utils.js";
+
+const html = document.querySelector("html");
+const toggleSwitch = document.querySelector(".js-theme-toggle-btn");
+const currentTheme = loadFromStorage("theme") || "dark";
+
+html.setAttribute("data-theme", currentTheme);
+
+toggleSwitch.addEventListener('click', () => {
+  
+  const currentTheme = loadFromStorage("theme") || "dark";
+
+  if(currentTheme === "dark"){
+    html.setAttribute("data-theme", "light");
+    saveToStorage("theme", "light");
+  }else if(currentTheme === "light"){
+    html.setAttribute("data-theme", "dark");
+    saveToStorage("theme", "dark");
+  }
+
+});
